13c19a2061c92664ef22994f4fc6b12381962baf
Kael <i@kael.me>
2015-10-15 18:06:45 +0800

    main functionalities

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-[![NPM version](https://badge.fury.io/js/read-neuron-json.svg)](http://badge.fury.io/js/read-neuron-json)
-[![npm module downloads per month](http://img.shields.io/npm/dm/read-neuron-json.svg)](https://www.npmjs.org/package/read-neuron-json)
-[![Build Status](https://travis-ci.org/neuron-js/read-neuron-json.svg?branch=master)](https://travis-ci.org/neuron-js/read-neuron-json)
-[![Dependency Status](https://david-dm.org/neuron-js/read-neuron-json.svg)](https://david-dm.org/neuron-js/read-neuron-json)
+[![NPM version](https://badge.fury.io/js/neuron-json.svg)](http://badge.fury.io/js/neuron-json)
+[![npm module downloads per month](http://img.shields.io/npm/dm/neuron-json.svg)](https://www.npmjs.org/package/neuron-json)
+[![Build Status](https://travis-ci.org/neuron-js/neuron-json.svg?branch=master)](https://travis-ci.org/neuron-js/neuron-json)
+[![Dependency Status](https://david-dm.org/neuron-js/neuron-json.svg)](https://david-dm.org/neuron-js/neuron-json)
 
-# read-neuron-json
+# neuron-json
 
 The utility to read neuron.json
 
 ## Install
 
 ```sh
-$ npm install read-neuron-json --save
+$ npm install neuron-json --save
 ```
 
 ## Usage
 
 ```js
-var read = require('read-neuron-json');
+var read = require('neuron-json');
 var cwd = '/path/to/package';
 read(cwd, function (err, json){
   // ...
---
+++ index.js
@@ -0,0 +1,86 @@
+// utility tools for neuron.json
+
+'use strict';
+
+var fse         = require('fs-extra');
+var fs          = require('fs');
+var node_path   = require('path');
+var cleaner     = require('./lib/clean');
+var comment_json = require('comment-json');
+
+
+// Cleans the neuron.json for legacy
+exports.clean = function (cwd, json, callback) {
+  cleaner.clean(cwd, json, callback);
+};
+
+
+exports.read = function(cwd, callback) {
+  var file = node_path.join(cwd, 'neuron.json');
+  exports._read_json(file, function (err, json) {
+    if (err) {
+      return callback(err);
+    }
+
+    cleaner.clean(cwd, json, callback);
+  });
+};
+
+
+exports.write = function(cwd, json, callback) {
+  var file = node_path.join(cwd, 'neuron.json');
+  exports._save_to_file(file, pkg, callback);
+};
+
+
+exports._save_to_file = function(file, json, callback) {
+  fse.outputFile(file, comment_json.stringify(json, null, 2), function(err) {
+    callback(err && {
+      code: 'ERROR_SAVE_PKG',
+      message: 'fail to save package to "' + file + '", error: ' + err.stack,
+      data: {
+        error: err,
+        file: file
+      }
+    });
+  });
+};
+
+
+exports._read_json = function(file, callback) {
+  function cb (err, pkg) {
+    if (err) {
+      return callback({
+        code: 'ERROR_READ_JSON',
+        message: 'Error reading "' + file + '": \n' + err.stack,
+        data: {
+          error: err
+        }
+      });
+    }
+
+    callback(null, pkg);
+  }
+
+  fs.exists(file, function (exists) {
+    if (!exists) {
+      return callback(null, {});
+    }
+
+    fs.readFile(file, function (err, content) {
+      if (err) {
+        return cb(err);
+      }
+
+      var pkg;
+      try {
+        pkg = comment_json.parse(content.toString());
+      } catch(e) {
+        return cb(e);
+      }
+
+      cb(null, pkg);
+    });
+  });
+};
+
